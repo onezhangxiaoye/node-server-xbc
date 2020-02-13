738264a7fe6b2cff6146e4c122877a244e4683b7
yuehaiming <915873514@qq.com>
2020-02-13 18:11:27 +0800

    update  user login verification

--- routes/users.js
+++ routes/users.js
@@ -2,12 +2,59 @@ var express = require('express');
 var router = express.Router();
 var multiparty = require('multiparty');
 
+const userList = [
+    {
+        userName: 'admin',
+        password: '123123',
+    },
+    {
+        userName: '12317xiang',
+        password: '123456',
+    },
+    {
+        userName: 'xiaobaicai',
+        password: '666666',
+    },
+];
+
+function errSend(code, message, res){
+    res.send({
+        code,
+        message,
+    });
+    res.end();
+}
+
 
 /**
  * 用户登录
  */
 router.post('/', function (req, res, next) {
     let {userName, password} = req.body;
+
+    if(userName.length < 3 || userName.length > 12){
+        let message = userName.length === 0 ? '用户名不能为空！' : (userName.length > 12 ? '用户名长度不能大于12位！' : '用户名必须大于3位！');
+        errSend(1, message, res);
+        return;
+    }
+    if(password.length < 6 || password.length > 18){
+        let message =password.length === 0 ? '密码不能为空！' : (userName.length > 18 ? '密码长度不能大于18位！' : '密码长度必须大于6位！');
+        errSend(2, message, res);
+        return;
+    }
+
+    let user = userList.find(v => v.userName === userName);
+    if(!user){
+        errSend(3, '用户名不存在', res);
+        return;
+    }
+    if(user.password !== password){
+        errSend(4, '密码错误', res);
+        return;
+    }
+
+
+
     console.log(userName, password);
     res.cookie('userName', userName, {httpOnly: true});
     res.cookie('password', password, {httpOnly: true});
