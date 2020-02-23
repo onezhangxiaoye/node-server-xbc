9c45b614211cb64774932ff437f198cf60f9f026
yuehaiming <915873514@qq.com>
2020-02-23 22:18:34 +0800

    update response data

--- routes/users.js
+++ routes/users.js
@@ -17,35 +17,44 @@ const userList = [
     },
 ];
 
-function errSend(code, message, res) {
+function responseSend(code, message, res, data) {
     res.send({
         code,
         message,
+        data,
     });
     res.end();
 }
 
 router.post('/register',function (req, res, next) {
     let {userName, password} = req.body;
+    if(userName === undefined){
+        responseSend(1, '未接收到用户名参数', res);
+        return;
+    }
+    if(password === undefined){
+        responseSend(1, '未接收到密码参数', res);
+        return;
+    }
 
     if (userName.length < 3 || userName.length > 12) {
         let message = userName.length === 0 ? '用户名不能为空！' : (userName.length > 12 ? '用户名长度不能大于12位！' : '用户名必须大于3位！');
-        errSend(1, message, res);
+        responseSend(1, message, res);
         return;
     }
     if (password.length < 6 || password.length > 18) {
         let message = password.length === 0 ? '密码不能为空！' : (userName.length > 18 ? '密码长度不能大于18位！' : '密码长度必须大于6位！');
-        errSend(2, message, res);
+        responseSend(2, message, res);
         return;
     }
     let user = userList.find(v => v.userName === userName);
     if (user) {
-        errSend(3, '用户名已存在', res);
+        responseSend(3, '用户名已存在', res);
         return;
     }
     userList.push({userName, password});
 
-    errSend(0, '注册成功', res);
+    responseSend(0, 'success', res);
 
 });
 
@@ -56,35 +65,42 @@ router.post('/register',function (req, res, next) {
 router.post('/', function (req, res, next) {
     let {userName, password} = req.body;
 
+    if(userName === undefined){
+        responseSend(1, '未接收到用户名参数', res);
+        return;
+    }
+    if(password === undefined){
+        responseSend(1, '未接收到密码参数', res);
+        return;
+    }
+
     if (userName.length < 3 || userName.length > 12) {
         let message = userName.length === 0 ? '用户名不能为空！' : (userName.length > 12 ? '用户名长度不能大于12位！' : '用户名必须大于3位！');
-        errSend(1, message, res);
+        responseSend(1, message, res);
         return;
     }
     if (password.length < 6 || password.length > 18) {
         let message = password.length === 0 ? '密码不能为空！' : (userName.length > 18 ? '密码长度不能大于18位！' : '密码长度必须大于6位！');
-        errSend(2, message, res);
+        responseSend(2, message, res);
         return;
     }
 
     let user = userList.find(v => v.userName === userName);
     if (!user) {
-        errSend(3, '用户名不存在', res);
+        responseSend(3, '用户名不存在', res);
         return;
     }
     if (user.password !== password) {
-        errSend(4, '密码错误', res);
+        responseSend(4, '密码错误', res);
         return;
     }
-
 
     console.log(userName, password);
     res.cookie('userName', userName, {httpOnly: true});
     res.cookie('password', password, {httpOnly: true});
     res.cookie('money', 0, {httpOnly: true});
 
-    res.send({userName, password});
-    res.end();
+    responseSend(0, 'success', res, {userName, password});
 });
 router.get('/', function (req, res, next) {
     // console.log(req.body);
@@ -96,22 +112,16 @@ router.get('/', function (req, res, next) {
  * 查询用户信息
  */
 router.post('/userInfo', function (req, res, next) {
-    console.log(req.cookies);
-
     let {userName, password, money} = req.cookies;
     if (!userName || !password) {
-        res.send({
-            message: '未登录！'
-        });
+        responseSend(0, '未登录！', res);
     } else {
-        console.log(userName, password,);
-        res.send({
+        responseSend(0, 'success', res, {
             userName,
             password,
             money,
         });
     }
-    res.end();
 });
 
 router.post('/formData', function (req, res, next) {
@@ -139,20 +149,23 @@ router.post('/pay', function (req, res, next) {
             message: '未登录！'
         });
     } else {
-        console.log(/^\d+$/.test(req.body.money));
+        if(req.body.money === undefined){
+            responseSend(1, '未接收到金额参数', res);
+            return;
+        }
         if (!(/^\d+$/.test(req.body.money))) {
-            errSend(1, '金额只能输入数字', res);
+            responseSend(1, '金额只能输入数字', res);
             return;
         }
         money = (+(money || 0)) + (+(req.body.money) || 0);
         res.cookie('money', money, {httpOnly: true});
-        res.send({
+
+        responseSend(0, 'success', res, {
             userName,
             password,
             money
         });
     }
-    res.end();
 });
 
 module.exports = router;
