a9364c624e1edd120f3d20cbad1d0bb1be1d657d
yuehaiming <915873514@qq.com>
2020-03-03 22:49:20 +0800

    update 新增mysql配置，users方面接口都和数据库对接完成

--- routes/users.js
+++ routes/users.js
@@ -1,21 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var multiparty = require('multiparty');
-
-const userList = [
-    {
-        userName: 'admin',
-        password: '123123',
-    },
-    {
-        userName: '12317xiang',
-        password: '123456',
-    },
-    {
-        userName: 'xiaobaicai',
-        password: '666666',
-    },
-];
+//数据库配置
+const mysql = require('../utils/mysql/mysql.js')();
 
 function responseSend(code, message, res, data) {
     res.send({
@@ -25,6 +12,12 @@ function responseSend(code, message, res, data) {
     });
     res.end();
 }
+
+router.get('/', function (req, res, next) {
+    // console.log(req.body);
+    // res.cookie('rememberme', '1', { maxAge: 900000, httpOnly: true });
+    res.render('index', {title: 'Express'});
+});
 
 router.post('/register',function (req, res, next) {
     let {userName, password} = req.body;
@@ -47,17 +40,26 @@ router.post('/register',function (req, res, next) {
         responseSend(2, message, res);
         return;
     }
-    let user = userList.find(v => v.userName === userName);
-    if (user) {
-        responseSend(3, '用户名已存在', res);
-        return;
-    }
-    userList.push({userName, password});
-
-    responseSend(0, 'success', res);
-
+    //检查用户名是否存在
+    let sql = "SELECT id FROM `users` WHERE user_name=?";
+    mysql.query(sql, [userName], (err, data) => {
+        console.log('检查用户名是否存在--',err, data);
+        if (Array.isArray(data) && data.length > 0){
+            responseSend(3, '用户名已存在', res);
+        }else{
+            //存如当前用户数据
+            sql = "INSERT INTO `users` (`user_name`,`password`) VALUES (?,?)";
+            mysql.query(sql, [userName, password], (err, data) => {
+                console.log('用户注册--',err, data);
+                if (data.affectedRows === 1){
+                    responseSend(0, 'success', res);
+                }else{
+                    responseSend(1, '异常', res, {err, data});
+                }
+            })
+        }
+    });
 });
-
 
 /**
  * 用户登录
@@ -85,27 +87,20 @@ router.post('/', function (req, res, next) {
         return;
     }
 
-    let user = userList.find(v => v.userName === userName);
-    if (!user) {
-        responseSend(3, '用户名不存在', res);
-        return;
-    }
-    if (user.password !== password) {
-        responseSend(4, '密码错误', res);
-        return;
-    }
-
-    console.log(userName, password);
-    res.cookie('userName', userName, {httpOnly: true});
-    res.cookie('password', password, {httpOnly: true});
-    res.cookie('money', 0, {httpOnly: true});
-
-    responseSend(0, 'success', res, {userName, password});
-});
-router.get('/', function (req, res, next) {
-    // console.log(req.body);
-    // res.cookie('rememberme', '1', { maxAge: 900000, httpOnly: true });
-    res.render('index', {title: 'Express'});
+    //检查用户名是否存在
+    let sql = "SELECT id, user_name, password, money FROM `users` WHERE user_name=?";
+    mysql.query(sql, [userName], (err, data) => {
+        console.log('检查用户名是否存在--',err, data);
+        if (!Array.isArray(data) || data.length === 0){
+            responseSend(3, '用户名不存在', res);
+        }else if(data[0].password !== password){
+            responseSend(4, '密码错误', res);
+        }else{
+            res.cookie('userName', userName, {httpOnly: true});
+            res.cookie('id', data[0].id, {httpOnly: true});
+            responseSend(0, 'success', res, {userName, password});
+        }
+    });
 });
 
 /**
@@ -124,6 +119,210 @@ router.post('/userInfo', function (req, res, next) {
     }
 });
 
+/**
+ * 充值
+ */
+router.post('/topUp', function (req, res, next) {
+    console.log('req.cookies---', req.cookies);
+    console.log('req.content-type---', req.headers['content-type']);
+    let {userName, id} = req.cookies;
+    if (!userName || !id) {
+        responseSend(1, '未登录！', res);
+    } else {
+        if(req.body.money === undefined){
+            responseSend(1, '未接收到金额参数', res);
+            return;
+        }
+        if (!(/^\d+$/.test(req.body.money))) {
+            responseSend(1, '金额只能输入数字', res);
+            return;
+        }
+
+        money = +(req.body.money) || 0;
+
+        //更新金额
+        let sql = "UPDATE `users` SET money=money+? WHERE id=?";
+        mysql.query(sql, [money, id], (err, data) => {
+            console.log('更新金额--',err, data);
+            if (data.affectedRows === 1){
+                responseSend(0, 'success', res);
+            }else{
+                responseSend(1, '异常', res, {err, data});
+            }
+        });
+    }
+});
+
+/**
+ * 购买支付
+ */
+router.post('/buy', async function (req, res, next) {
+    let {userName, id} = req.cookies;
+    if(!userName || !id){
+        responseSend(1, '未登录！', res);
+        return;
+    }
+    let {goodsList} = req.body;
+    if(!Array.isArray(goodsList) || goodsList.length === 0){
+        responseSend(1, '购买商品不能为空！', res);
+        return;
+    }
+    console.log(goodsList);
+    let count = 0;
+    goodsList.forEach(v => {
+        if(!(Object.prototype.toString.call(v) !== '[Object object]') || !v.goodsId || !v.goodsNum) count++;
+    });
+    if(count !== 0){
+        responseSend(1, '传入得商品参数异常！', res);
+        return;
+    }
+
+    let userMoney = await getUserMoneyById(id);
+    if(userMoney === 0){
+        responseSend(1, '余额不足！', res);
+        return;
+    }
+    
+    //检查商品的库存和余额是否可以完成支付
+    let countMoney = 0;
+    for (let item of goodsList){
+        let goods = await getGoodsById(item.goodsId);
+        if(!goods){
+           responseSend(1, `商品不存在！`, res);
+           return
+        }
+        countMoney += item.goodsNum * goods.price;
+        if(countMoney > userMoney){
+            responseSend(1, `您的余额不足了！`, res);
+            return;
+        }
+        if(item.goodsNum > goods.num){
+            responseSend(1, `商品-${goods.name}-的库存不足了！`, res);
+            return;
+        }
+    }
+    /**
+     * 开启事务
+     */
+    mysql.beginTransaction(async function (err){
+
+        for (let item of goodsList){
+            let error = await createOrder(id, item.goodsId, item.goodsNum);
+            if(error !== true){
+                responseSend(1, `下单失败！`, res);
+                return mysql.rollback(function() {
+                    throw error;
+                });
+            }
+            error = await updateGoodsNum(item.goodsId, item.goodsNum);
+            if(error !== true){
+                responseSend(1, `下单失败！`, res);
+                return mysql.rollback(function() {
+                    throw error;
+                });
+            }
+        }
+        let error = await updateUserMoney(id, countMoney);
+        if(error !== true){
+            responseSend(1, `下单失败！`, res);
+            return mysql.rollback(function() {
+                throw error;
+            });
+        }
+        mysql.commit(function(err) {
+            if (err) {
+                return mysql.rollback(function() {
+                    throw err;
+                });
+            }
+            responseSend(0, `success`, res);
+        });
+    });
+});
+
+async function updateUserMoney(id, money){
+    const sql = "UPDATE `users` SET money=money-? WHERE id=?;";
+    return new Promise(((resolve, reject) => {
+        mysql.query(sql, [money, id], (err, data) => {
+            console.log('更新用户余额--', data);
+            if(err){
+                resolve(err);
+            }else if (data.affectedRows === 1){
+                resolve(true);
+            }else{
+                resolve(new Error('未知异常！'));
+            }
+        });
+    }))
+}
+
+async function updateGoodsNum(goods_id, goods_num){
+    const sql = "UPDATE `goods` SET num=num-? WHERE id=?;";
+    return new Promise(((resolve, reject) => {
+        mysql.query(sql, [goods_num, goods_id], (err, data) => {
+            console.log('更新订单--', data);
+            if(err){
+                resolve(err);
+            }else if (data.affectedRows === 1){
+                resolve(true);
+            }else{
+                resolve(new Error('未知异常！'));
+            }
+        });
+    }))
+}
+
+async function createOrder(user_id, goods_id, goods_num){
+    const sql = "INSERT INTO `order` (user_id, goods_id, goods_num, create_time) VALUE (?, ?, ?, (SELECT CURRENT_TIMESTAMP()));";
+    return new Promise(((resolve, reject) => {
+        mysql.query(sql, [user_id, goods_id, goods_num], (err, data) => {
+            console.log('生成订单--', data);
+            if(err){
+                resolve(err);
+            }else if (data.affectedRows === 1){
+                resolve(true);
+            }else{
+                resolve(new Error('未知异常！'));
+            }
+        });
+    }))
+}
+
+/**
+ *
+ * @param id
+ * @returns {Promise<unknown>}
+ */
+async function getGoodsById(id){
+    const sql = "SELECT * FROM `goods` WHERE id=?";
+    return new Promise(((resolve, reject) => {
+        mysql.query(sql, [id], (err, data) => {
+            console.log('通过id查询商品--', err, data);
+            if (Array.isArray(data) && data.length > 0){
+                resolve(data[0])
+            }else{
+                resolve(false);
+            }
+        });
+    }))
+}
+
+async function getUserMoneyById(id){
+    const sql = "SELECT money FROM `users` WHERE id=?";
+    return new Promise(((resolve, reject) => {
+        mysql.query(sql, [id], (err, data) => {
+            console.log('getUserMoneyById', data);
+            if (Array.isArray(data) && data.length > 0){
+                resolve(data[0].money);
+            }else{
+                resolve(false);
+            }
+        });
+    }))
+}
+
+
+
 router.post('/formData', function (req, res, next) {
     // parse a file upload
     var form = new multiparty.Form();
@@ -138,34 +337,6 @@ router.post('/formData', function (req, res, next) {
         res.end();
     });
 
-});
-
-router.post('/pay', function (req, res, next) {
-    console.log('req.cookies---', req.cookies);
-    console.log('req.content-type---', req.headers['content-type']);
-    let {userName, password, money} = req.cookies;
-    if (!userName || !password) {
-        res.send({
-            message: '未登录！'
-        });
-    } else {
-        if(req.body.money === undefined){
-            responseSend(1, '未接收到金额参数', res);
-            return;
-        }
-        if (!(/^\d+$/.test(req.body.money))) {
-            responseSend(1, '金额只能输入数字', res);
-            return;
-        }
-        money = (+(money || 0)) + (+(req.body.money) || 0);
-        res.cookie('money', money, {httpOnly: true});
-
-        responseSend(0, 'success', res, {
-            userName,
-            password,
-            money
-        });
-    }
 });
 
 module.exports = router;
